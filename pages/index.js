1d2e934a1821bfdd14d7f362588af82e313f17b0
Kaeittiwut Thongwhan <kaeitiwutt@gmail.com>
2020-10-16 13:06:25 +0700

    add function for retrieve API data

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Head from 'next/head';
+import Axios from 'axios';
 import Layout from '../components/Layout';
+import Link from 'next/link';
 
 class Index extends Component {
+  static async getInitialProps() {
+    const res = await Axios.get('http://localhost:3001/blogs');
+    return { blogs: res.data };
+  }
+
+  renderBlogs = (blogs) => {
+    return blogs.map((blog) => {
+      return (
+        <div key={blog.id} className="col-6">
+          <img src={blog.thumbnail} className="img-fluid" />
+          <h4 className="mt-3">
+            <Link href={'/blog/detail?id=' + blog.id}>{blog.subject}</Link>
+          </h4>
+          <hr />
+          <p className="text-justify">{blog.intro}</p>
+        </div>
+      );
+    });
+  };
+
   render() {
-    return <Layout>Hello bloggers.</Layout>;
+    return (
+      <Layout>
+        <div className="container">
+          <div className="row">{this.renderBlogs(this.props.blogs)}</div>
+        </div>
+      </Layout>
+    );
   }
 }
 
